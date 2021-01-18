fdad266269124c6d4cb91acb8b1897cba448181e
book-of-kells <60078020+book-of-kells@users.noreply.github.com>
2021-01-18 14:53:13 -0800

    optimizing by skipping loops in 'areBusesConsecutive', printing average times for iterations

--- 2020/13/13.go
+++ 2020/13/13.go
@@ -8,6 +8,7 @@ import (
 	"math"
 	"net/http"
 	_ "net/http/pprof"
+	"time"
 )
 
 var VERBOSE = false
@@ -16,14 +17,13 @@ var VERBOSE = false
 type BusTimestamp struct {
 	bus int
 	timestamp int
-	elapsed int64
 	index int
 	modArr []int
 }
 
 
 func main() {
-
+	startTime := time.Now()
 	fptr := flag.String("file", "input.txt", "file path to read from")
 	vptr := flag.Bool("v", false, "verbose")
 	pprofPtr := flag.Bool("pprof", false, "profiling")
@@ -47,6 +47,10 @@ func main() {
 	maxBus, _ := getMaxBus(busArr)
 
 	checkBusMultiples(maxBus, *startAtTimestampPtr, busArr)
+
+	endTime := time.Now()
+	elapsed := endTime.Sub(startTime)
+	fmt.Printf("elapsed time: %v\n", elapsed)
 }
 
 func checkBusMultiples(busNum int, startAtTimestamp int, busArr []*int) {
@@ -57,18 +61,25 @@ func checkBusMultiples(busNum int, startAtTimestamp int, busArr []*int) {
 	if VERBOSE {
 		fmt.Printf("checkBusMultiples():\tfor bus %d starting time %d\n", busNum, earliest)
 	}
-	busPtr := &BusTimestamp{bus: busNum, timestamp: earliest, elapsed: 0}
+	busPtr := &BusTimestamp{bus: busNum, timestamp: earliest}
 	finished := checkBusTime(*busPtr, busArr, &busNum)
 
+	elapsedTotal := time.Duration(0)
+
 	for !finished {
-		if VERBOSE && i % 100000 == 0 {
-			// every 10,000 iterations, print an update
-			fmt.Printf("checkBusMultiples():\tfor bus %d checking time %d\n", busNum, earliest)
-		}
+		startTime := time.Now()
 		earliest = earliest + busNum
-		busPtr := &BusTimestamp{bus: busNum, timestamp: earliest, elapsed: 0}
+		busPtr := &BusTimestamp{bus: busNum, timestamp: earliest}
 		finished = checkBusTime(*busPtr, busArr, &busNum)
 		i++
+		endTime := time.Now()
+		elapsed := endTime.Sub(startTime)
+		elapsedTotal += elapsed
+		if VERBOSE && i % 1000000 == 0 {
+			// every 1,000,000 iterations, print an update
+			avgElapsed := elapsedTotal.Nanoseconds()/int64(i)
+			fmt.Printf("checkBusMultiples():\tfor bus %d checking time %d avg elapsed: %d ns\n", busNum, earliest, avgElapsed)
+		}
 	}
 }
 
@@ -83,6 +94,9 @@ func checkBusTime(b BusTimestamp, busArr []*int, maxBusNum *int) bool {
 	return finished
 }
 
+// busModInfoArr is an array of BusTimestamp instances.
+// The busModInfoArr doesn't have nil values because
+// each BusTimestamp instance has its own index property.
 func getBusModInfoArr(earliest int, busArr []*int, maxBusNum *int) []BusTimestamp {
 	var busModInfoArr []BusTimestamp
 
@@ -95,7 +109,6 @@ func getBusModInfoArr(earliest int, busArr []*int, maxBusNum *int) []BusTimestam
 		busInfo := BusTimestamp{
 			bus: bus,
 			timestamp: earliest,
-			elapsed: 0,
 			index: idx,
 			modArr: make([]int, m),
 		}
@@ -123,30 +136,45 @@ func getBusModInfoArr(earliest int, busArr []*int, maxBusNum *int) []BusTimestam
 func busesAreConsecutive(busInfoArr []BusTimestamp) (bool, *int) {
 
 	firstBus := busInfoArr[0] // assumes first element in busArr != nil
+	isTrue := true
+	isFalse := false
 
 	for _, firstBusMod := range firstBus.modArr {
 		firstBusModDiff := firstBusMod - firstBus.index
-		boolArr := make([]bool, len(busInfoArr[1:]))
+		isConsecutiveArr := make([]*bool, len(busInfoArr[1:]))
 
 		// check the nextBus in the busInfoArr
 		for nextBusIdx, nextBus := range busInfoArr[1:] {
-
+			if nextBusIdx > 0 && isConsecutiveArr[nextBusIdx-1] != nil && *(isConsecutiveArr[nextBusIdx-1]) == false {
+				// if even one value in isConsecutiveArr is false, stop checking the rest of the `nextBus`es
+				break
+			}
 			// check all the mods for the nextBus
 			for _, nextBusMod := range nextBus.modArr {
 				if nextBusMod - nextBus.index == firstBusModDiff {
-					boolArr[nextBusIdx] = true
+					isConsecutiveArr[nextBusIdx] = &isTrue
 					break // skip to checking the next nextBus
+				} else {
+					isConsecutiveArr[nextBusIdx] = &isFalse // continue checking this nextBus's mods
 				}
 				if nextBusMod - nextBus.index > firstBusModDiff {
-					boolArr[nextBusIdx] = false // continue checking this nextBus's mods
+					// each BusTimestamp's mod in its modArr increases with the modArr index
+					// so if the current nextBusMod is so large that
+					// nextBusMod - nextBus.index > firstBusModDiff
+					// then there is no need to check the rest of the mods in the modArr
+					if *(isConsecutiveArr[nextBusIdx]) == false {
+						// this will always be the case
+					}
+					break
 				}
+
 			} // check the next mod of this nextBus
 
 		} // check the next nextBus
 
 		allConsecutive := true
-		for _, isConsecutive := range boolArr {
-			if isConsecutive == false {
+		for _, isConsecutive := range isConsecutiveArr {
+			if *isConsecutive == false {
 				allConsecutive = false
 				break
 			}
